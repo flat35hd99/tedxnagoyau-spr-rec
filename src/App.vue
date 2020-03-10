32d678eb7b7ef314cded20b74096e66263d42b06
WataruSugiura <38929316+flat35hdapp@users.noreply.github.com>
2020-03-10 16:25:42 +0900

    20200309Deploy

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@
                   <v-img :src="imgLink.group2017" alt=""></v-img>
                   <v-card-title>about us</v-card-title>
                   <v-card-subtitle>TEDxNagoyaUについて</v-card-subtitle>
-                  <v-card-text>TEDxNagoyaUは学生のみで運営されるTED公認のTEDxイベント運営団体です。プレゼンの内容をスピーカーと一緒に考えたり、協力してくださる企業の方々を探したり、マーケティングを行ったりと、イベント運営に必要なことをすべて自分たちで行っています。TEDxNagoyaU 2020に向けて、私たちと一緒に最高のイベントを作り上げませんか？</v-card-text>
+                  <v-card-text>TEDxNagoyaUは学生のみで運営されるTED公認のTEDxイベント運営団体です。スピーカーを外部からお呼びし、プレゼンテーションを一緒に考えたり、協力してくださる企業の方々を探したり、マーケティングを行ったりと、イベント運営に必要なことをすべて自分たちで行っています。TEDxNagoyaU 2020に向けて、私たちと一緒に最高のイベントを作り上げませんか？</v-card-text>
                   <!--画像に写ってる人、今誰も活動してない件-->
                 </v-card>
               </div>
@@ -122,44 +122,80 @@
           </v-col>
         </v-row>
         <v-row>
-          <v-col xs="12">
+          <v-col>
             <div>
               <v-card :color="gray">
                 <v-card-title>Info</v-card-title>
                 <v-card-subtitle>迷っているあなたへ</v-card-subtitle>
                 <v-card-text>サークル・部活選びの参考に、TEDxNagoyaUの特徴やメンバーの声をまとめました！</v-card-text>
-                <v-row justify="left" class="mx-1">
-                  <v-col xs="6" sm="6" md="4">
+                <v-row justify="left">
+                  <v-col xs="12" sm="6">
+                    <div>
+                      <v-card>
+                        <v-card-title>メンバーの声</v-card-title>
+                        <!--<v-card-subtitle></v-card-subtitle>
+                        <v-card-text>hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge</v-card-text>-->
+                        <v-card-text>
+                          <v-row>
+                            <v-expansion-panels>
+                              <v-expansion-panel
+                                v-for="(item, i) in memberPanels"
+                                :key="i"
+                                hide-actions
+                              >
+                                <v-expansion-panel-header>
+                                  <v-row align="center" class="spacer" no-gutters>
+                                    <v-col xs="1" sm="3" md="2" class="hidden-xs-only">
+                                      <v-avatar size="48px">
+                                        <img :src="item.avator" alt="Avatar">
+                                      </v-avatar>
+                                    </v-col>
+                                    <v-col xs="4" sm="9" md="10">
+                                      <p></p>
+                                      <strong v-html="item.name"></strong>
+                                      <p >{{item.excerpt}}</p>
+                                    </v-col>
+                                  </v-row>
+
+                                </v-expansion-panel-header>
+                                <v-expansion-panel-content>
+                                  <v-divider></v-divider>
+                                  <v-card-text style="white-space: pre-line;">{{item.message}}</v-card-text>
+                                </v-expansion-panel-content>
+                              </v-expansion-panel>
+                            </v-expansion-panels>
+                          </v-row>
+                        </v-card-text>
+                      </v-card>
+                    </div>
+                  </v-col>
+                  <!--<v-col xs="12" sm="6" md="6">
                     <div>
                       <v-card class="">
                         <v-card-title>こんな人に入って欲しい！</v-card-title>
                         <v-card-subtitle></v-card-subtitle>
-                        <v-card-text>hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge</v-card-text>
+                        <v-card-text>大学生活、何もせずに過ごしたくない！一味違ったことをしたい！と思っているそこのあなた！！一度説明会に足を運んでみませんか？？17人の個性豊かなメンバーで、お待ちしています！</v-card-text>
                       </v-card>
                     </div>
-                  </v-col>
-                  <v-col xs="6" sm="6" md="4">
+                  </v-col>-->
+                  <v-col xs="12" sm="6">
                     <div>
-                      <v-card class="">
-                        <v-card-title>メンバーの声</v-card-title>
+                      <v-card class="mb-6">
+                        <v-card-title>こんな人に入って欲しい！</v-card-title>
                         <v-card-subtitle></v-card-subtitle>
-                        <v-card-text>hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge</v-card-text>
+                        <v-card-text>大学生活、何もせずに過ごしたくない！一味違ったことをしたい！と思っているそこのあなた！！一度説明会に足を運んでみませんか？？17人の個性豊かなメンバーで、お待ちしています！</v-card-text>
                       </v-card>
                     </div>
-                  </v-col>
-                  <v-col xs="6" sm="6" md="4">
                     <div>
-                      <v-card class="">
+                      <v-card class="mb-6" v-if="false">
                         <v-card-title>参加のメリット</v-card-title>
                         <v-card-subtitle></v-card-subtitle>
                         <v-card-text>hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge</v-card-text>
                       </v-card>
                     </div>
-                  </v-col>
-                  <v-col xs="6" sm="6" md="4">
                     <div>
-                      <v-card class="">
-                        <v-card-title>参加のデメリット</v-card-title>
+                      <v-card v-if="false">
+                        <v-card-title>参加のメリット</v-card-title>
                         <v-card-subtitle></v-card-subtitle>
                         <v-card-text>hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge</v-card-text>
                       </v-card>
@@ -170,7 +206,6 @@
             </div>
           </v-col>
         </v-row>
-
         <v-row>
           <v-col xs=12>
             <v-card :color="gray">
@@ -264,19 +299,19 @@ export default {
           subtitle: "スピーカーによるトーク",
           img: "https://live.staticflickr.com/1764/43251114892_a7f2f6c16a_k.jpg",
           //img: firstView,
-          text: "7~9人の",
+          text: "7~9人のスピーカーがプレゼンテーションを行います。Technology Entertainment Design (TED)とあるように、様々なトピックでアイデアをプレゼンします。",
         },
         {
           title: "Break Session",
           subtitle: "トークに集中した後に",
           img: "https://live.staticflickr.com/65535/49596215708_44d57902fe_k.jpg",
-          text: "hogehoge",
+          text: "トーク内容について参加者同士で考えたり、イベントのテーマについて深く考えることのできる時間です。トークの時間では伝えきれなかったスピーカーの熱い思いを聞くこともできます。",
         },
         {
           title: "After Party",
           subtitle: "参加者同士でイベントの感想を共有",
           img: "https://live.staticflickr.com/65535/48261788467_da6b0df5fd_k.jpg",
-          text: "hogehoge",
+          text: "1日の終わりに、今日のイベントを食事と一緒に参加者同士で振り返ります。",
         }
       ],
       teamContents: [
@@ -375,8 +410,45 @@ export default {
           link: "https://www.facebook.com/tedxnagoyau/"
         },
       ],
-      gray: "#FAFAFA"
+      memberPanels: [
+        {
+          avator: "http://drive.google.com/uc?export=view&id=15xgHinkKTvtDk8ctNfNuDW8V6O6gHEW0",
+          name: "上田蓮",
+          excerpt: "TEDxの世界へようこそ！",
+          message: "この団体では、アイデアとアイデア、人と人が繋がる瞬間を目の当たりにすることができます。熱意ある仲間と一緒に、多くの困難の先にある奇跡を見てみませんか？\n何かを学びたい、作り上げたい、成果を残したいと思う人は是非一度足を運んでみてください！\nお待ちしてます！"
+        },
+        {
+          avator: "http://drive.google.com/uc?export=view&id=13NWXJ-6u6ig44cwlZOWsdLHwJod5RjMZ",
+          name: "弓達汰一",
+          excerpt: "ひと味違った大学生活に",
+          message: "この団体は大学に入って、周りの人とはひと味違った経験をしたいという人にオススメです。\n多くの大学生はバイトやサークルに明け暮れる生活を送っています。僕の周りにもバイトしかする事ないよーって行ってる人はたくさんいます。しかしこの団体に所属すると、大学の外の人たちと関わりを持つ機会が増えたり、自分の軸を持って行動しているような志の高い人とたくさん出会えます。\n特に自分の活動してるコミュニケーションチームは社会人との関わりも多く、実際に企業に行って交渉をするなど、大学生ではできないような経験ができます。\n周りとはひと味違う充実した大学生活を送ってみませんか。"
+        },
+
+        {
+          avator: "http://drive.google.com/uc?export=view&id=1BWaRN7g-IhoC9M5qRw3pdVfn89RiKvxU",
+          name: "山崎紗耶",
+          excerpt: "切磋琢磨できる仲間と出会える場",
+          message: "みなさんは、今までの人生を振り返って、影響の大きかったものってなんだと思いますか？\n私は、“友達・仲間”だと思っています。私は高校時代、友達と切磋琢磨して勉強していたし、仲間と一生懸命になって部活動を頑張っていた。友達、仲間がいるからこそ、頑張ることができた。こう考えると、みなさんの人生には、友達や仲間が大きな影響を及ぼしてはいなかったでしょうか？\n今、何かに挑戦しようと思っている人、成し遂げたいことのある人、貴重な大学生活を無駄にしたくない人、TEDxNagoyaUで、そんなあなたに素敵な影響を及ぼしてくれる、切磋琢磨できる仲間ときっと出会えるはずです。\nTEDxNagoyaUには、日々何かに挑戦して頑張っている仲間、そして、挑戦を応援してくれる仲間がたくさんいます。もちろん、TEDxNagoyaUの活動をする上でも、挑戦はたくさんあります。私自身、TEDxNagoyaUで挑戦する仲間と出会って、もっと頑張ろうと思うことがたくさんあります。\nそんな仲間とこのTEDxNagoyaUで出会って、あなたの大学生活ををもっと充実させませんか？"
+        },
+      ],
+      gray: "#FAFAFA",
+
     }
+  },
+  created: async function () {
+    this.$ga.page('/tedxnagoyau-rec-spr.html')
   }
+  /*head: {
+    title: function(){
+      return {
+        inner: "TEDxNagoyaU"
+      },
+      script: [
+        {
+          type: 'text/javascript', src: '"https://www.googletagmanager.com/gtag/js?id=UA-119308617-1', async: true
+        }
+      ]
+    }
+  }*/
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
+//import VueHead from 'vue-head'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-119308617-1'
+})
+//Vue.use(VueHead)
 
 new Vue({
   vuetify,
